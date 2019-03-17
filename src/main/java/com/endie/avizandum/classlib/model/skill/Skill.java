94a9be4a5cb7c5ff46aa2502da55f2a5a7e6f161
endieposts <endie@endie.net>
2019-03-17 18:42:17 +0000

    Creates classlib for avizandum with exported objects

---
+++ src/main/java/com/endie/avizandum/classlib/model/skill/Skill.java
@@ -0,0 +1,83 @@
+package com.endie.avizandum.classlib.model.skill;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.endie.avizandum.classlib.model.actor.ActorType;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+
+@Entity
+@Table(name="SKILLS")
+@JsonRootName(value = "skill",namespace = "skills")
+public class Skill {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	@Size(max = 100)
+	private String name;
+	private Integer difficulty;
+	private String description;
+	
+	@ManyToMany
+	@JsonManagedReference
+	private Collection<ActorType> allowedActorTypes;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Integer difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public void setAllowedActorTypes(Collection<ActorType> allowedActorTypes) {
+		this.allowedActorTypes = allowedActorTypes;
+	}
+
+	public Collection<ActorType> getAllowedActorTypes() {
+		return allowedActorTypes;
+	}
+
+	public Skill() {
+		this.allowedActorTypes = new ArrayList<ActorType>();
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
